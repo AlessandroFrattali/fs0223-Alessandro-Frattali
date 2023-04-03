3b17978e27d00a707790049542e892dd5f3babfe
AlessandroFrattali <alessandro.frattali@gmail.com>
2023-04-03 16:29:18 +0200

    First commit

---
+++ m1/s2/g1/JS/altro.js
@@ -0,0 +1,8 @@
+var nome = prompt("Inserisci il tuo nome"); 
+    console.log(nome);
+    inserimento = true;
+    if (inserimento){
+        document.write("Benvenuto "+nome + ", come stai oggi?");
+    }
+    
+
---
+++ m1/s2/g1/JS/index.js
@@ -0,0 +1,13 @@
+//function somma (){
+    //var a = 4;
+    //var b = 8;
+    //var c = a+b;
+    //alert("Il risultato dell'operazione è " +c);
+//}
+var numero = prompt  ("Inserisci un numero");
+       console.log(numero);
+       if (numero <= 10){
+        document.write("Hai scelto un numero che arriva fino a 10");
+       } else {
+        document.write("Hai scelto un numero più grande di 10");
+       }
